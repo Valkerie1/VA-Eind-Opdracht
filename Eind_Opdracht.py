b6c0160955c4ea943277e8e16009a9fabf8e0d44
Valkerie1 <91312670+Valkerie1@users.noreply.github.com>
2021-11-04 10:04:29 +0100

    Update Eind_Opdracht.py

--- Eind_Opdracht.py
+++ Eind_Opdracht.py
@@ -12,7 +12,7 @@ import folium
 st.set_page_config(page_title = 'Streamlit Dashboard', layout= 'wide')
 
 
-
+# inladen van WHO data + bewerking
 who_data = pd.read_csv('countries.csv')
 
 who_data.drop(['Arable (%)', 'Crops (%)', 'Other (%)', 'Agriculture', 'Industry', 'Service', 'Climate'], axis=1, inplace=True)
@@ -26,5 +26,312 @@ who_data['Phones (per 1000)'] = who_data['Phones (per 1000)'].str.replace(',', '
 who_data['Birthrate'] = who_data['Birthrate'].str.replace(',', '.').astype(float)
 who_data['Deathrate'] = who_data['Deathrate'].str.replace(',', '.').astype(float)
 
-st.table(who_data)
+mappings = {'ASIA (EX. NEAR EAST)         ':"ASIA",
+           'EASTERN EUROPE                     ':"EUROPE",
+           'NORTHERN AFRICA                    ':"AFRICA",
+           'OCEANIA                            ':"OCEANIA",
+           'WESTERN EUROPE                     ':"EUROPE",
+           'SUB-SAHARAN AFRICA                 ':"AFRICA",
+           'LATIN AMER. & CARIB    ':"LATIN AMERICA",
+           'C.W. OF IND. STATES ':"CIS",
+           'NEAR EAST                          ':"MIDDLE EAST",
+           'NORTHERN AMERICA                   ':"NORTHERN AMERICA",
+           'BALTICS                            ':"EUROPE"}
+who_data['Region'] = who_data['Region'].replace(mappings)
+
+
+
+# Inladen van geojson data + bewerking
+countries = gpd.read_file('countries.geojson')
+who_data['Country'] = who_data['Country'].str.replace(' ','')
+countries['ADMIN'] = countries['ADMIN'].str.replace(' ','')
+
+remap = {"UnitedStates" : "UnitedStatesofAmerica"}
+who_data['Country'] = who_data['Country'].replace(remap)
+
+WHO = gpd.GeoDataFrame(pd.merge(df, countries, 
+                                left_on="Country", right_on='ADMIN'))
+
+ASIA = WHO[WHO['Region']=='ASIA']
+EUROPE = WHO[WHO['Region']=='EUROPE']
+AFRICA = WHO[WHO['Region']=='AFRICA']
+OCEANIA = WHO[WHO['Region']=='OCEANIA']
+LATINAMERICA = WHO[WHO['Region']=='LATIN AMERICA']
+CIS = WHO[WHO['Region']=='CIS']
+MIDDLEEAST = WHO[WHO['Region']=='MIDDLE EAST']
+NORTHERNAMERICA = WHO[WHO['Region']=='NORTHERN AMERICA']
+
+
+
+# Inladen van leeftijdsverdeling per land + bewerking
+world_age_categories = pd.read_csv('world_age_categories.csv')
+
+
+
+# Boxplot
+fig_boxplot = px.box(data_frame=df, x=df['Region'], y='GDP ($ per capita)', 
+             color='Region')
+fig_boxplot.update_xaxes(title_text = 'Regio')
+fig_boxplot.update_yaxes(title_text = 'GDP ($ per inwoner)')
+fig_boxplot.update_layout({'title':{'text':'GDP per regio', 
+                            'x':0.5}})
+    
+fig_boxplot.show()
+
+
+
+
+
+# Scatterplots
+fig_scatter_GDP_InfantMortality = px.scatter(data_frame=df,
+                x='GDP ($ per capita)',
+                y='Infant mortality (per 1000 births)',
+                trendline='ols', 
+                trendline_options=dict(log_y=True),
+                trendline_color_override="red")
+
+fig_scatter_GDP_InfantMortality.update_xaxes(title_text = 'GDP ($ per inwoner)')
+fig_scatter_GDP_InfantMortality.update_yaxes(title_text = 'Kindersterfte (per 1000 geboortes)')
+fig_scatter_GDP_InfantMortality.update_layout({'title':{'text':'Relatie tussen GDP en kindersterfte', 
+                                                               'x':0.5}})
+    
+fig_scatter_GDP_InfantMortality.show()
+
+
+
+fig_scatter_GDP_Phones = px.scatter(data_frame=df,
+                x='GDP ($ per capita)',
+                y='Phones (per 1000)',
+                trendline='ols',
+                trendline_color_override="red")
+
+fig_scatter_GDP_Phones.update_xaxes(title_text = 'GDP ($ per inwoner)')
+fig_scatter_GDP_Phones.update_yaxes(title_text = 'Aantal telefoons (per 1000 inwoners)')
+fig_scatter_GDP_Phones.update_layout({'title':{'text':'Relatie tussen GDP en aantal telefoons per 1000 inwoners', 
+                                                      'x':0.5}})
+    
+fig_scatter_GDP_Phones.show()
+
+
+
+fig_scatter_GDP_Literacy = px.scatter(data_frame=df,
+                x='GDP ($ per capita)',
+                y='Literacy (%)',
+                trendline='ols',
+                trendline_options=dict(log_x=True),
+                trendline_color_override="red")
+
+fig_scatter_GDP_Literacy.update_xaxes(title_text = 'GDP ($ per inwoner)')
+fig_scatter_GDP_Literacy.update_yaxes(title_text = 'Geletterdheid (in %)')
+fig_scatter_GDP_Literacy.update_layout({'title':{'text':'Relatie tussen GDP en de geletterdheid', 
+                                                        'x':0.5}})
+    
+fig_scatter_GDP_Literacy.show()
+
+
+
+
+
+
+# Creeren van kaarten
+# Wereld
+m = folium.Map(zoom_control = True,
+tiles = 'cartodb positron')
+
+m.choropleth(
+    geo_data = WHO,
+    name = 'geometry',
+    data = WHO,
+    columns = ["Country", "GDP ($ per capita)"],
+    key_on = 'feature.properties.Country',
+    line_opacity = 0.5,
+    fill_opacity = 0.75,
+    fill_color = 'YlOrRd',
+    legend_name = "GDP ($ per capita)")
+
+m
+
+# Asia
+mASIA = folium.Map(zoom_control = False, zoom_start=3, location=[28.0893191, 105.1101691],
+tiles = 'cartodb positron')
+
+mASIA.choropleth(
+    geo_data = ASIA,
+    name = 'geometry',
+    data = ASIA,
+    columns = ["Country", "GDP ($ per capita)"],
+    key_on = 'feature.properties.Country',
+    line_opacity = 0.5,
+    fill_opacity = 0.75,
+    fill_color = 'YlOrRd',
+    legend_name = "GDP ($ per capita)")
+
+mASIA
+
+#Europe
+mEUROPE = folium.Map(zoom_control = False, zoom_start=3, location=[54.0893191, 25.1101691],
+tiles = 'cartodb positron')
+
+mEUROPE.choropleth(
+    geo_data = EUROPE,
+    name = 'geometry',
+    data = EUROPE,
+    columns = ["Country", "GDP ($ per capita)"],
+    key_on = 'feature.properties.Country',
+    line_opacity = 0.5,
+    fill_opacity = 0.75,
+    fill_color = 'YlOrRd',
+    legend_name = "GDP ($ per capita)")
+
+mEUROPE
+
+#Africa
+mAFRICA = folium.Map(zoom_control = False, zoom_start=3, location=[0.0893191, 15.1101691],
+tiles = 'cartodb positron')
+
+mAFRICA.choropleth(
+    geo_data = AFRICA,
+    name = 'geometry',
+    data = AFRICA,
+    columns = ["Country", "GDP ($ per capita)"],
+    key_on = 'feature.properties.Country',
+    line_opacity = 0.5,
+    fill_opacity = 0.75,
+    fill_color = 'YlOrRd',
+    legend_name = "GDP ($ per capita)")
+
+mAFRICA
+
+#Oceania
+mOCEANIA = folium.Map(zoom_control = False, zoom_start=3, location=[-35.0893191, 140.1101691],
+tiles = 'cartodb positron')
+
+mOCEANIA.choropleth(
+    geo_data = OCEANIA,
+    name = 'geometry',
+    data = OCEANIA,
+    columns = ["Country", "GDP ($ per capita)"],
+    key_on = 'feature.properties.Country',
+    line_opacity = 0.5,
+    fill_opacity = 0.75,
+    fill_color = 'YlOrRd',
+    legend_name = "GDP ($ per capita)")
+
+mOCEANIA
+
+#Latin America
+mLATINAMERICA = folium.Map(zoom_control = False, zoom_start=3, location=[-18, -60.1101691],
+tiles = 'cartodb positron')
+
+mLATINAMERICA.choropleth(
+    geo_data = LATINAMERICA,
+    name = 'geometry',
+    data = LATINAMERICA,
+    columns = ["Country", "GDP ($ per capita)"],
+    key_on = 'feature.properties.Country',
+    line_opacity = 0.5,
+    fill_opacity = 0.75,
+    fill_color = 'YlOrRd',
+    legend_name = "GDP ($ per capita)")
+
+mLATINAMERICA
+
+#Commonwealth of independant states (CIS)
+mCIS = folium.Map(zoom_control = False, zoom_start=3, location=[65.0893191, 100.1101691],
+tiles = 'cartodb positron')
+
+mCIS.choropleth(
+    geo_data = CIS,
+    name = 'geometry',
+    data = CIS,
+    columns = ["Country", "GDP ($ per capita)"],
+    key_on = 'feature.properties.Country',
+    line_opacity = 0.5,
+    fill_opacity = 0.75,
+    fill_color = 'YlOrRd',
+    legend_name = "GDP ($ per capita)")
+
+mCIS
+
+#Middle east
+mMIDDLEEAST = folium.Map(zoom_control = False, zoom_start=4, location=[30.0893191, 45.1101691],
+tiles = 'cartodb positron')
+
+mMIDDLEEAST.choropleth(
+    geo_data = MIDDLEEAST,
+    name = 'geometry',
+    data = MIDDLEEAST,
+    columns = ["Country", "GDP ($ per capita)"],
+    key_on = 'feature.properties.Country',
+    line_opacity = 0.5,
+    fill_opacity = 0.75,
+    fill_color = 'YlOrRd',
+    legend_name = "GDP ($ per capita)")
+
+mMIDDLEEAST
+
+#Noth America
+mNORTHERNAMERICA = folium.Map(zoom_control = False, zoom_start=2, location=[70.0893191, -110.1101691],
+tiles = 'cartodb positron')
+
+mNORTHERNAMERICA.choropleth(
+    geo_data = NORTHERNAMERICA,
+    name = 'geometry',
+    data = NORTHERNAMERICA,
+    columns = ["Country", "GDP ($ per capita)"],
+    key_on = 'feature.properties.Country',
+    line_opacity = 0.5,
+    fill_opacity = 0.75,
+    fill_color = 'YlOrRd',
+    legend_name = "GDP ($ per capita)")
+
+mNORTHERNAMERICA
+
+
+
+
+
+
+# Leeftijds piramide
+country_input = 'Belgium'
+
+y_range = world_age_categories[world_age_categories['Country']==country_input]['Age']
+men = world_age_categories[world_age_categories['Country']==country_input]['Perc_men']
+women = world_age_categories[world_age_categories['Country']==country_input]['Perc_women']
+
+layout_range = [-round(men.max(),0)-2, round(women.max(),0)+2]
+layout_tick_vals = [-round(men.max(),0), -round(men.max()/2,0) , 0 , round(women.max()/2,0) , round(women.max(),0)]
+layout_tick_text = [-round(men.max(),0), -round(men.max()/2,0) , 0 , round(women.max()/2,0) , round(women.max(),0)]
+
+layout = go.Layout(yaxis=go.layout.YAxis(title='Leeftijd'),
+                   xaxis=go.layout.XAxis(
+                       range=layout_range,
+                       tickvals=layout_tick_vals,
+                       ticktext=layout_tick_text,
+                       title='Percentage van de bevolking'),
+                   title={'text': 'Verdeling van de bevolking over leeftijds categorieën', 'x' : 0.5},
+                   barmode='overlay',
+                   bargap=0.1)
+
+data = [go.Bar(y=y_range,
+               x=-men,
+               orientation='h',
+               name='Men',
+               text= men.astype('float'),
+               textposition='outside',
+               hoverinfo= 'text',
+               marker=dict(color='powderblue')
+               ),
+        go.Bar(y=y_range,
+               x=women,
+               orientation='h',
+               name='Women',
+               text= women.astype('float'),
+               textposition='outside',
+               hoverinfo= 'text',
+               marker=dict(color='hotpink')
+               )]
+
+pyo.iplot(dict(data=data, layout=layout))
+
 
